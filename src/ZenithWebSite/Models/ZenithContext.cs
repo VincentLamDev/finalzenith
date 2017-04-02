b316e563e2226e4bf461780114e551001d04ea50
Vincent Lam <vincentlam060@gmail.com>
2017-04-02 15:19:56 -0700

    first push

---
+++ src/ZenithWebSite/Models/ZenithContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZenithWebSite.Models
+{
+    public class ZenithContext : DbContext
+    {
+        public ZenithContext(DbContextOptions<ZenithContext> options) : base(options)
+        { }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
